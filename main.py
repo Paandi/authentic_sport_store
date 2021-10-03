05c686bee00ffb558017fb774b45b56e8ba8bfc2
Dinesh C Pandey1 <Dinesh.Pandey1@telusinternational.com>
2021-10-03 23:04:00 +0530

    change python version from 3.6.3 to 3.6.15 as per Heroku requirement

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 from Resource.User import UserRegister
 from Resource.Item import Items, ItemAll
-from db import db
 
 app = Flask(__name__)
 
@@ -16,16 +15,9 @@ app.secret_key = 'SportsStore'
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 api.add_resource(ItemAll, '/allitems')
 api.add_resource(UserRegister, '/signup')
 api.add_resource(Items, '/item/<string:name>')
 
 if __name__ == '__main__':
-    db.init_app(app)
     app.run(port=5000, debug=True)
